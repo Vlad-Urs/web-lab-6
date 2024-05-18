4918282330418ebe66b7c34990fec9e96f58a822
Vlad-Urs <113233447+Vlad-Urs@users.noreply.github.com>
2024-05-18 14:07:04 +0300

    added light/dark toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useState} from 'react'
 import './App.css'
-import bron from './assets/the_bronze.png'
+import { Toggle } from './components/Toggle';
+import useLocalStorage from 'use-local-storage';
+//import bron from './assets/the_bronze.png'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [isDark,setIsDark] = useLocalStorage("isDark",false);
 
   return (
-    <>
-      <img src={bron} alt="king" />
-      <h1>The Bronze Jade</h1>
-    </>
-  )
-}
-
-export default App
+    <div className="App" data-theme = {isDark ? "dark" : "light"}>
+      <Toggle
+      isChecked={isDark}
+      handleChange={()=>setIsDark(!isDark)}
+      />
+      <h1 className="title">Hello world!</h1>
+      <div className="box">
+        <h2>This is a box</h2>
+      </div>
+    </div>
+  );
+};
