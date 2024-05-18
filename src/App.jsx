8576cec7e959119ca5da408a2f123e15ae087341
Vlad-Urs <113233447+Vlad-Urs@users.noreply.github.com>
2024-05-18 17:24:29 +0300

    added main cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 import { Toggle } from './components/Toggle';
 import useLocalStorage from 'use-local-storage';
 import bron from './assets/the_bronze.png'
+import Card from './components/MainCards';
 
 
 export const App = () => {
   const [isDark,setIsDark] = useLocalStorage("isDark",false);
-
+  const [ppg, setPPG] = useLocalStorage('ppg', 16.2);
+      const [rpg, setRPG] = useLocalStorage('rpg', 11.8);
+      const [apg, setAPG] = useLocalStorage('apg', 8.5);
   return (
     <div className="App" data-theme = {isDark ? "dark" : "light"}>
       
@@ -16,9 +19,25 @@ export const App = () => {
         <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
       </header>
 
-      <h1 className="title">Hello world!</h1>
-      <div className="box">
-        <h2>This is a box</h2>
+      <div className='cards-container'>
+      <Card
+          title="PPG"
+          value={ppg}
+          growth="Since last month"
+          growthPercentage="18.2%"
+        />
+        <Card
+          title="APG"
+          value={apg}
+          growth="Since last month"
+          growthPercentage="28.4%"
+        />
+        <Card
+          title="RPG"
+          value={rpg}
+          growth="Since last month"
+          growthPercentage="18.2%"
+        />
       </div>
     </div>
   );
