a7ef3d79ce57353b1dd3d76fe256e1aed761f6f3
Vlad-Urs <113233447+Vlad-Urs@users.noreply.github.com>
2024-05-18 15:26:23 +0300

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import './App.css'
 import { Toggle } from './components/Toggle';
 import useLocalStorage from 'use-local-storage';
-//import bron from './assets/the_bronze.png'
+import bron from './assets/the_bronze.png'
 
 
 export const App = () => {
@@ -10,10 +10,12 @@ export const App = () => {
 
   return (
     <div className="App" data-theme = {isDark ? "dark" : "light"}>
-      <Toggle
-      isChecked={isDark}
-      handleChange={()=>setIsDark(!isDark)}
-      />
+      
+      <header className="app-header">
+      <h1 className="logo">HOOPALYTIX</h1>
+        <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+      </header>
+
       <h1 className="title">Hello world!</h1>
       <div className="box">
         <h2>This is a box</h2>
