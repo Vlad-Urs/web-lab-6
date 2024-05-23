17f65ce19b5431bdc01e9e0c07686256b5f6ab3a
Vlad-Urs <113233447+Vlad-Urs@users.noreply.github.com>
2024-05-23 12:21:07 +0300

    add jwt for swagger

--- back-end/app.py
+++ back-end/app.py
@@ -4,7 +4,7 @@ from init_db import init_database
 from models.database import db
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
-
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +13,14 @@ def create_app():
     # Configure SQLAlchemy to use SQLite
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:goatedpassword@localhost:5432/electro_scooterdb'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///matches_database.db'
+
+    # Init the JWT
+    # Configuration
+    app.config['SECRET_KEY'] = 'very-secret-key'
+    app.config["JWT_SECRET_KEY"] = "jwt-key"
+    app.config['JWT_TOKEN_LOCATION'] = ['headers']
+
+    jwt = JWTManager(app)
 
     db.init_app(app)
 
--- back-end/routes.py
+++ back-end/routes.py
@@ -3,16 +3,30 @@ from flask import request, jsonify
 from models.database import db
 from models.match import Match
 from __main__ import app
-from datetime import datetime
+from datetime import datetime, timedelta
+from flask_jwt_extended import create_access_token, get_jwt_identity,jwt_required, get_jwt
+
+
+@app.route('/token', methods=['GET'])
+def get_token():
+    if request.method == 'GET':
+        access_token = create_access_token(
+            identity="admin", 
+            expires_delta=timedelta(minutes=5),
+            additional_claims={'role': request.args.get("role", type=str)},
+        )
+        return jsonify({"jwt": access_token}), 200
 
 
 @app.route('/api/matches', methods=['GET'])
+@jwt_required()
 def get_matches():
     matches = Match.query.all()
     return jsonify([match.serialize() for match in matches]), 200
 
 
 @app.route('/api/matches', methods=['POST'])
+@jwt_required()
 def create_match():
     try:
         # Get data from the request body
@@ -34,6 +48,7 @@ def create_match():
     
 
 @app.route('/api/matches/<int:match_id>', methods=['GET'])
+@jwt_required()
 def get_match_by_id(match_id):
     # Find the Match by ID
     match = Match.query.get(match_id)
@@ -44,6 +59,7 @@ def get_match_by_id(match_id):
     
 
 @app.route('/api/matches/<int:match_id>', methods=['PUT'])
+@jwt_required()
 def update_match(match_id):
     try:
         # Find the Match by ID
@@ -67,6 +83,7 @@ def update_match(match_id):
     
 
 @app.route('/api/matches/<int:match_id>', methods=['DELETE'])
+@jwt_required()
 def delete_match(match_id):
     try:
         # Find the Match by ID
